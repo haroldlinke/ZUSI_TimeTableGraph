64a1eaff759e890b1a5de88f5af78f2973b4b5c6
haroldlinke <43720258+haroldlinke@users.noreply.github.com>
2022-04-28 17:31:12 +0200

    Delete python4delphi_call_timetablegraph.py

--- python4delphi_call_timetablegraph.py
+++
@@ -1,3 +0,0 @@
-from timetablepages.ZUSI_timetablegraph_main import main
-mainfile_dir = r"D:\data\doc\Embarcadero\Studio\Projekte\delphi4python\FirstExample\Win32\Debug\python-3.9.1"
-main(mainfile_dir)
