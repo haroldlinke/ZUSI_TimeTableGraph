4da8a0872879808cb13aedf888bb6a75049a4df7
Harold Linke <harold.linke@t-online.de>
2022-04-28 17:59:29 +0200

    V03.14b

--- timetablepages/ZUSI_timetablegraph_main.py
+++ timetablepages/ZUSI_timetablegraph_main.py
@@ -62,6 +62,7 @@ from timetablepages.ZUSI_TCP_class import ZUSI_TCP
 #from datetime import datetime
 import zipfile
 import urllib
+import shutil
 
 
 # ------------------------------
@@ -428,7 +429,48 @@ class TimeTableGraphMain(tk.Tk):
         self.update()
         
     
-        #StatusMsg_UserForm.Set_ActSheet_Label(P01.Format(int(time.time()) - M37.Update_Time, 'hh:mm:ss')+"\n"+str(a*b))    
+        #StatusMsg_UserForm.Set_ActSheet_Label(P01.Format(int(time.time()) - M37.Update_Time, 'hh:mm:ss')+"\n"+str(a*b))  
+        
+    def copytree(self, src, dst, symlinks=False, ignore=None):
+        names = os.listdir(src)
+        if ignore is not None:
+            ignored_names = ignore(src, names)
+        else:
+            ignored_names = set()
+
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+
+        errors = []
+        for name in names:
+            if name in ignored_names:
+                continue
+            srcname = os.path.join(src, name)
+            dstname = os.path.join(dst, name)
+            try:
+                if symlinks and os.path.islink(srcname):
+                    linkto = os.readlink(srcname)
+                    os.symlink(linkto, dstname)
+                elif os.path.isdir(srcname):
+                    self.copytree(srcname, dstname, symlinks, ignore)
+                else:
+                    shutil.copy2(srcname, dstname)
+                # XXX What about devices, sockets etc.?
+            except (IOError, os.error) as why:
+                errors.append((srcname, dstname, str(why)))
+            # catch the Error from the recursive copytree so that we can
+            # continue with other files
+            except BaseException as err:
+                errors.extend(err.args[0])
+        try:
+            shutil.copystat(src, dst)
+        except WindowsError:
+            # can't copy file access times on Windows
+            pass
+        except OSError as why:
+            errors.extend((src, dst, str(why)))
+        if errors:
+            raise Error(errors)     
 
     def Updateprogram(self):
         
